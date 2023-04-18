df74ce504590329bc2fbb9484ffe55e2b3fbe9e8
dongne-joomin <donghunc88@gmail.com>
2023-04-18 14:21:56 +0900

    doJoin 구현 중

---
+++ src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
@@ -0,0 +1,32 @@
+package com.koreaIT.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.koreaIT.demo.service.MemberService;
+import com.koreaIT.demo.vo.Member;
+
+@Controller
+public class UsrMemberController {
+
+	private MemberService memberService;
+
+	@Autowired
+	public UsrMemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
+	@RequestMapping("/usr/member/doJoin")
+	@ResponseBody
+	public Member doAdd(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+
+		memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+
+		int id = memberService.getLastInserId();
+
+		return memberService.getMemberById(id);
+	}
+
+}
