ad87b4c9defaec50e5150fe1f96f25e6f3e089d2
dongne-joomin <donghunc88@gmail.com>
2023-05-12 15:58:46 +0900

    NeedLogoutInterceptor

--- src/main/java/com/koreaIT/demo/config/MyWebMvcConfigurer.java
+++ src/main/java/com/koreaIT/demo/config/MyWebMvcConfigurer.java
@@ -8,50 +8,58 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.koreaIT.demo.interceptor.BeforeActionInterceptor;
 import com.koreaIT.demo.interceptor.NeedLoginInterceptor;
+import com.koreaIT.demo.interceptor.NeedLogoutInterceptor;
 
 @Configuration
 public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
 	private BeforeActionInterceptor beforeActionInterceptor;
 	private NeedLoginInterceptor needLoginInterceptor;
+	private NeedLogoutInterceptor needLogoutInterceptor;
 
 	@Autowired
 	public MyWebMvcConfigurer(BeforeActionInterceptor beforeActionInterceptor,
-			NeedLoginInterceptor needLoginInterceptor) {
+			NeedLoginInterceptor needLoginInterceptor, NeedLogoutInterceptor needLogoutInterceptor) {
 		this.beforeActionInterceptor = beforeActionInterceptor;
 		this.needLoginInterceptor = needLoginInterceptor;
+		this.needLogoutInterceptor = needLogoutInterceptor;
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-
+		
 		InterceptorRegistration ir;
 		
 		ir = registry.addInterceptor(beforeActionInterceptor);
 		ir.addPathPatterns("/**");
-		ir.addPathPatterns("/favicom.ico");
+		ir.addPathPatterns("/favicon.ico");
 		ir.excludePathPatterns("/resource/**");
 		
 		ir = registry.addInterceptor(needLoginInterceptor);
 		ir.addPathPatterns("/usr/article/write");
-		ir.addPathPatterns("/usr/article/modify");
 		ir.addPathPatterns("/usr/article/doWrite");
+		ir.addPathPatterns("/usr/article/modify");
 		ir.addPathPatterns("/usr/article/doModify");
 		ir.addPathPatterns("/usr/article/doDelete");
 		ir.addPathPatterns("/usr/member/doLogout");
+		ir.addPathPatterns("/usr/member/myPage");
+		ir.addPathPatterns("/usr/member/checkPassword");
+		ir.addPathPatterns("/usr/member/doCheckPassword");
+		ir.addPathPatterns("/usr/member/doModify");
+		ir.addPathPatterns("/usr/member/passwordModify");
+		ir.addPathPatterns("/usr/member/doPasswordModify");
+		ir.addPathPatterns("/usr/reactionPoint/getReactionPoint");
 		ir.addPathPatterns("/usr/reactionPoint/doInsertReactionPoint");
 		ir.addPathPatterns("/usr/reactionPoint/doDeleteReactionPoint");
 		ir.addPathPatterns("/usr/reply/doWrite");
-		ir.addPathPatterns("/usr/reply/doModify");
 		ir.addPathPatterns("/usr/reply/doDelete");
+		ir.addPathPatterns("/usr/reply/doModify");
 		ir.addPathPatterns("/usr/reply/getReplyContent");
 		
-		
-//		registry.addInterceptor(beforeActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");
-//
-//		registry.addInterceptor(needLoginInterceptor).addPathPatterns("/usr/article/write")
-//				.addPathPatterns("/usr/article/doWrite").addPathPatterns("/usr/article/doDelete")
-//				.addPathPatterns("/usr/article/doModify").addPathPatterns("/usr/article/modify")
-//				.addPathPatterns("/usr/article/doLogout");
+		ir = registry.addInterceptor(needLogoutInterceptor);
+		ir.addPathPatterns("/usr/member/doJoin");
+		ir.addPathPatterns("/usr/member/login");
+		ir.addPathPatterns("/usr/member/doLogin");
 	}
 
 }
